4b74a7b65f607f4ff03b6a9c210affb70ac5015a
gurmindersingh751 <gurminder290195@gmail.com>
2021-03-03 21:02:35 -0500

    Added new project for creating APIs

---
+++ Sohi.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sohi.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly AuthenticationController ac;
+        public AuthenticationController(AuthenticationController ac)
+        {
+            this.ac = ac;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetUser()
+        {
+            try
+            {
+                return Ok(await ac.GetUser());
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retrieving data from the database");
+            }
+        }
+
+    }
+}
