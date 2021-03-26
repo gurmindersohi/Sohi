33c97a43d8ec26a19e965dbb077051a32b18c073
Gurminder singh <gurminder290195@gmail.com>
2021-03-26 08:46:40 -0400

    Fixing Facebook Login Button issue

--- Sohi.Web/Sohi.Web/Controllers/MarketingController.cs
+++ Sohi.Web/Sohi.Web/Controllers/MarketingController.cs
@@ -24,12 +24,13 @@ namespace Sohi.Web.Controllers
             return View();
         }
 
-        public async Task<IActionResult> GetUserAccessTokenAsync()
+        public async Task<string> GetUserAccessTokenAsync()
         {
 
             string appID = "1133254180447928";
             string appSecret = "89698c449ef21f306c26f2435f046ec6";
             string redirectUrl = "https://localhost:5001/Marketing/Index";
+
 
             HttpClient client = new HttpClient();
 
@@ -54,89 +55,23 @@ namespace Sohi.Web.Controllers
                 response.EnsureSuccessStatusCode();
                 string responseBody = await response.Content.ReadAsStringAsync();
 
-
+                return responseBody;
             }
             catch (Exception ex)
             {
+                return ex.Message;
             }
 
-            return View("Index");
 
         }
 
         public async Task<IActionResult> FacebookLoginAsync()
         {
 
-           await GetUserAccessTokenAsync();
+           string token = await GetUserAccessTokenAsync();
 
-           
-
-
-
-
-            //try
-            //{
-
-            //    HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
-            //    request.Method = "GET";
-            //    using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
-            //    {
-            //        StreamReader reader = new StreamReader(response.GetResponseStream());
-
-            //        string values = reader.ReadToEnd();
-
-            //        JObject jObject = JObject.Parse(values);
-            //        var response2 = (string)jObject.SelectToken("code");
-            //    }
-            //}
-            //catch (Exception ex)
-            //{
-
-            //}
-
-
-
-
-            //try
-            //{
-            //    string url = string.Format("https://graph.facebook.com/v6.0/oauth/access_token?client_id={0}&redirect_uri={1}&client_secret={2}&code={3}", appID, redirectURL, appSecret, code);
-
-            //    HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
-            //    request.Method = "GET";
-            //    using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
-            //    {
-            //        StreamReader reader = new StreamReader(response.GetResponseStream());
-
-            //        string values = reader.ReadToEnd();
-
-            //        JObject jObject = JObject.Parse(values);
-            //        user_access_token = (string)jObject.SelectToken("access_token");
-            //    }
-            //}
-            //catch
-            //{
-            //    user_access_token = null;
-            //}
-
-
-            //string url = "https://www.facebook.com/v10.0/dialog/oauth?client_id=1133254180447928&redirect_uri=https://localhost:5001/Marketing&scope=email";
-            //Response.Redirect(url);
-
-            //if (Request["code"] != null)
-            //{
-            //    string code = Request.QueryString["code"];
-            //    Session["accessToken"] = GetUserAccessToken(appID, appSecret, redirectUrl, code);
-            //    Session.Timeout = 1;
-
-            //    String x = "<script type='text/javascript'>window.opener.location.href='AdCampaigns.aspx?rdk=" + "&admin=" + Session["admin"].ToString() + "#Intro';self.close();</script>";
-            //    ScriptManager.RegisterClientScriptBlock(this.Page, this.Page.GetType(), "script", x, false);
-
-
-
-
-            //}
-
-            return View();
+            return View("Index");
         }
+
     }
 }
