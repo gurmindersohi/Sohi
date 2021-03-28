95d6be1960ba796cf93b9f0e2594a5083aa6e1e5
gurmindersingh751 <gurminder290195@gmail.com>
2021-03-28 14:19:53 -0400

    Added/Update Users Migration

---
+++ Sohi.Web/Sohi.Web/Migrations/20210328181809_Extend_Users.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Sohi.Web.Migrations
+{
+    public partial class Extend_Users : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AccountId",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AccountId",
+                table: "Users");
+        }
+    }
+}
